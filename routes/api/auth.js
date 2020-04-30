51bc572d4911e4b75d0d77f9f547b723e7266c82
ValeedAnjum <allinone644@gmail.com>
2020-04-30 02:33:25 -0700

    with details comments

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -21,6 +21,7 @@ router.post('/register', [
     check('email', 'Email is required').isEmail(),
     check('password', 'password is required').isLength({min: 6})
 ], async(req, res) => {
+    //checking for errors
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res
@@ -29,22 +30,27 @@ router.post('/register', [
                 errors: errors.array()
             });
     }
+    //getting data from request body
     const {name, email, password} = req.body;
+
+    //getting user dumy image url
     const avatar = config.get("userImage");
     try {
+      //searching if user is already registered with email
       let user = await User.findOne({email});
       if(user){
         return res.status(400).json({errors:[{msg:'User already exists'}]});
-      } 
+      }
       user = new User({
         name,
         email,
         password,
         avatar
       });
-      
+      //encryption of password through bcryptjs package
       const salt = await bcrypt.genSalt(10);
       user.password = await bcrypt.hash(password,salt);
+      //saving user credentials in the database
       await user.save();
 
       const payload = {
@@ -52,7 +58,7 @@ router.post('/register', [
           id:user.id
         }
       }
-
+      //configuring and get json web token
       jwt.sign(
         payload,
         config.get("jwtSecret"),
@@ -75,18 +81,20 @@ router.post('/signin', [
   check('email','Email is required').isEmail(),
   check('password','password is required').not().isEmpty()
 ] ,async (req,res) => {
+  //checking for errors
   const errors = validationResult(req);
   if(!errors.isEmpty()){
     return res.status(400).json({errors:[{'msg':'Invaild credentials'}]});
   }
   const {email,password} = req.body;
   try {
-    
+    //finding the user
     const user = await User.findOne({email});
+    //if user not exists
     if(!user){
       return res.status(400).json({errors:[{msg:'Invalid credentials'}]});
     }
-    
+    //matching user enterd password with database password
     const isMatch = await bcrypt.compare(password,user.password);
     if(!isMatch){
       return res.status(400).json({errors:[{msg:'Invalid credentials'}]});
@@ -97,7 +105,7 @@ router.post('/signin', [
         id:user.id
       }
     }
-
+    //configuring and get json web token
     jwt.sign(
       payload,
       config.get('jwtSecret'),
