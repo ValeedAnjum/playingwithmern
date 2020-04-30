b0908b57bb3285ce06e99ac406740d74f8bbc209
ValeedAnjum <allinone644@gmail.com>
2020-04-30 02:25:32 -0700

    sign in route

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -67,4 +67,48 @@ router.post('/register', [
     }
 })
 
+//@route    POST api/signin 
+//@des      authenticate user and get token
+//@access   Public
+
+router.post('/signin', [
+  check('email','Email is required').isEmail(),
+  check('password','password is required').not().isEmpty()
+] ,async (req,res) => {
+  const errors = validationResult(req);
+  if(!errors.isEmpty()){
+    return res.status(400).json({errors:[{'msg':'Invaild credentials'}]});
+  }
+  const {email,password} = req.body;
+  try {
+    
+    const user = await User.findOne({email});
+    if(!user){
+      return res.status(400).json({errors:[{msg:'Invalid credentials'}]});
+    }
+    
+    const isMatch = await bcrypt.compare(password,user.password);
+    if(!isMatch){
+      return res.status(400).json({errors:[{msg:'Invalid credentials'}]});
+    }
+
+    const payload = {
+      user:{
+        id:user.id
+      }
+    }
+
+    jwt.sign(
+      payload,
+      config.get('jwtSecret'),
+      {expiresIn:36000},
+      (err,token) => {
+        if(err) throw err;
+        res.json({token});
+      }
+    )
+  } catch (err) {
+    res.status(500).send('Server Error');
+  }
+})
 module.exports = router;
