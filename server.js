98c4d521f941b6d6f821643c3f4ef2d159a5651b
ValeedAnjum <allinone644@gmail.com>
2020-04-29 23:48:00 -0700

    end playingwithmern now going to develop an app with authentication json web token and posting, retrieving,updating and deleting some dummy data

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json({extended:false}));
 app.get('/',(req,res) => res.send('API Running P'));
 
 //define routes
-app.use('/api/items',require('./routes/api/items'));
+app.use('/api/auth',require('./routes/api/auth'));
 
 const PORT = process.env.PORT || 5000;
 
